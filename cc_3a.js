2dc62aab5681a64914a42c2b7e51e1207ef8454f
emma <eleyva2@usf.edu>
2025-09-13 19:17:25 -0700

    Final commit containing project changes

--- cc_3a.js
+++ cc_3a.js
@@ -6,20 +6,22 @@ let customers = [
     {name: "Emma" , email: "emmaleyva@gmail.com" , purchases: "scissors , pencils , expomarkers"},
 ];
 
-// Summary Of Products 
-customers.forEach(obj => {
-    console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchases}`);
-});
-
-// Adding and Removing Customers 
+// Adding and Removing Data
 customers.push({name:"Sophia", email: "sophialeyva.com", purchases: "sticky notes, erasers, thumbtacks"});
 customers.shift(); // Removes the first customer (Jannet)
 
-
+// Update Customer Information
 customers.forEach(item => {
   if (item.name === "David") {
-    item.email = "davidlederer"; // Directly modify the email (notice that the last letter of the email is now "r")
+    item.email = "davidlederer@gmail.com"; // Directly modify the email (notice that the last letter of the email is now "r")
   }
 });
 
+customers[0].purchases = "tape, paper, paperweights, glue";
+
 console.log(customers);
+
+//Displaying Customer Information
+customers.forEach(obj => {
+    console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchases}`);
+});
