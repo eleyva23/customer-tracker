06d25bfb517288986110cf78482920de4f52e95a
emma <eleyva2@usf.edu>
2025-09-13 18:57:33 -0700

    Updated commit to test cc_3a.js

--- cc_3a.js
+++ cc_3a.js
@@ -1,15 +1,25 @@
 // Coding Challange 3a
- // Create Dataset
- let customers = [
-    {name: "Jannet", email: "jannetlouis.com" , purchases: "pens , staples  ,paperclips"}, 
-    {name: "David" , email: "davidledere.com", purchases: "tape , paper , paperweights"}, 
-    {name: "Emma" , email: "emmaleyva.com" , purchases: "scissors , pencils , expomarkers"},
+// Create Dataset
+let customers = [
+    {name: "Jannet", email: "jannetlouis@gmail.com" , purchases: "pens , staples  ,paperclips"}, 
+    {name: "David" , email: "davidledere@gmail.com", purchases: "tape , paper , paperweights"}, 
+    {name: "Emma" , email: "emmaleyva@gmail.com" , purchases: "scissors , pencils , expomarkers"},
+];
 
- ];
-
- // Summary Of Products 
- console.log(customers.forEach(obj => console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchases}`)))
+// Summary Of Products 
+customers.forEach(obj => {
+    console.log(`${obj.name}\t|\t${obj.email}\t|\t${obj.purchases}`);
+});
 
 // Adding and Removing Customers 
-customers.push({name:"sophia", email: "sophialeyva.com" , purchases: "sticky notes, erasers, thumbtacks"})
- customers.pull({name:"sophia", email: "sophialeyva.com" , purchases: "sticky notes, erasers, thumbtacks"})
+customers.push({name:"Sophia", email: "sophialeyva.com", purchases: "sticky notes, erasers, thumbtacks"});
+customers.shift(); // Removes the first customer (Jannet)
+
+
+customers.forEach(item => {
+  if (item.name === "David") {
+    item.email = "davidlederer"; // Directly modify the email (notice that the last letter of the email is now "r")
+  }
+});
+
+console.log(customers);
